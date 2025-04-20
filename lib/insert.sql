d8c26e62a55bd32d4a0bca592224c5b9d6365d42
Oketch <oketchngwani@gmail.com>
2025-04-20 13:59:00 +0900

    push

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 5, 'M', 'brown', 'calm', 1),
+('Rowdy', 3, 'M', 'black', 'rowdy', 1),
+('Tabitha', 6, 'F', 'gray', 'gentle', 1),
+('Sergeant Brown', 8, 'M', 'brown', 'strict', 1),
+('Melissa', 2, 'F', 'white', 'playful', 1),
+('Grinch', 10, 'M', 'green', 'grumpy', 0),
+('Wendy', 4, 'F', 'brown', 'friendly', 1),
+(NULL, 7, 'M', 'black', 'unknown', 1);
